1b04d685208ec2544315765a8ff961ab03348fee
subinjohn <subin.john@nike.com>
2013-09-13 10:32:35 -0700

    Initial Commit

---
+++ TestProj/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestProj
+//
+//  Created by Pete Salvo on 9/13/13.
+//  Copyright (c) 2013 Pete Salvo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestProj/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestProj
+//
+//  Created by Pete Salvo on 9/13/13.
+//  Copyright (c) 2013 Pete Salvo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
